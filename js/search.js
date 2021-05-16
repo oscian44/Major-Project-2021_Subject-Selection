551ba93682ea846c6c3bebcc79ddfbbece4349a0
oscian <oscar@reinitz.com.au>
2021-05-17 09:27:21 +1000

    Search JS setup
    Very early structure of JS binary search created. Array constructed

---
+++ js/search.js
@@ -0,0 +1,55 @@
+var subjects = [
+    {name:"eStud",},
+    {name:"eEald"},
+    {name:"eStand"},
+    {name:"eAdv"},
+    {name:"eExt1"},
+    {name:"eExt2"},
+    {name:"mStand"},
+    {name:"mAdv"},
+    {name:"mExt1"},
+    {name:"mExt2"},
+    {name:"dram"},
+    {name:"ecBus"},
+    {name:"ecEco"},
+    {name:"geo"},
+    {name:"hAnc"},
+    {name:"hExt"},
+    {name:"hLeg"},
+    {name:"hMod"},
+    {name:"lExt"},
+    {name:"lFre"},
+    {name:"lChi"},
+    {name:"lLat"},
+    {name:"lGre"},
+    {name:"mu1"},
+    {name:"mu2"},
+    {name:"muExt"},
+    {name:"pdhpe"},
+    {name:"pSr1"},
+    {name:"pSr2"},
+    {name:"sBio"},
+    {name:"sChe"},
+    {name:"sEes"},
+    {name:"sPhy"},
+    {name:"sExt"},
+    {name:"tDt"},
+    {name:"tEs"},
+    {name:"tIt"},
+    {name:"tIpt"},
+    {name:"tSdd"},
+    {name:"va"}
+];
+var selectedSub = [];
+
+
+function searchSub(searchTerm){
+
+}
+
+function subjectPick(inputSub){
+
+
+
+
+}
