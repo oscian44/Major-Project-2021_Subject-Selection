39d458b7a55ac917eddbfc048d43267401e3ea75
oscian <oscar@reinitz.com.au>
2021-06-02 12:50:34 +1000

    Significant development to binary search. Implemented div hiding and search algorithm. Search term simplification WIP

--- js/search.js
+++ js/search.js
@@ -119,21 +119,56 @@ var subjects = [{
         name: "va"
     }
 ];
+
 var selectedSub = [];
+
+var subjectHeaders = ["headEn", "headMa", "headD", "headEc", "headG", "headH", "headL", "headMu", "headPdhpe", "headS", "headT", "headVa"]
 
 
 
 //Binary Search Function for the list of subjects
 function searchSub(searchTerm) {
-
+    var tempTerm = ""
+    var searchResult = ""
     //Sorts subjects alphabetically prior to binary search
     sortsubjects()
 
+    //Convert search term to its relative subject code for use in binary search
+    tempTerm = simpTerm(searchTerm)
 
-    //Binary Search Function
-    
+    //Executes binary Search Function
+    searchResult = binarySearch(subjects, tempTerm)
+
+    hideDivs()
+
+    document.getElementById(subjects.name[searchResult]).style.display = "Block"
+
 }
 
+//Binary Search Function
+function binarySearch(items, value) {
+    var startIndex = 0
+    var endIndex = items.length - 1
+    var middle = Math.floor((endIndex + startIndex) / 2)
+
+    while (items.name[middle] != value && startIndex < endIndex) {
+
+        //Changes search location
+        if (value < items.name[middle]) {
+            endIndex = middle - 1;
+        } else if (value > items.name[middle]) {
+            endIndex = middle + 1;
+        }
+
+        //Returns to centre of search
+        middle = Math.floor((endIndex + startIndex) / 2);
+    }
+
+    //Assures value is correct and returns it
+    return (items.name[middle] != value) ? -1 : middle;
+}
+
+//Selects user's desired subject and adds to the selected list
 function subjectPick(inputSub) {
 
 
@@ -141,7 +176,31 @@ function subjectPick(inputSub) {
 
 }
 
-//Sorts alphabetically sorts the list of subjects
+//Simplifies the search term to its relative code
+function simpTerm(term) {
+    term = term.toLowerCase
+    switch (term) {
+        case "english studies":
+            return ("enStud");
+        case "english eal/d":
+        case "english eald":
+            return ("enEald");
+        case "english standard":
+        case "english (standard)":
+            return("enStand");
+        case "english advanced":
+        case "english (advanced)":
+            return("enAdv");
+        case "english extension 1":
+            return("enExt1");
+        case "english extension 2":
+            return("enExt2");
+    }
+}
+
+
+
+//Sorts alphabetically the list of subjects
 function sortsubjects() {
     subjects.sort(function (a, b) {
         var x = a.name.toLowerCase();
@@ -156,4 +215,24 @@ function sortsubjects() {
     });
 
     console.log(subjects)
+}
+
+//Hides all subject divs
+function hideDivs() {
+    for (i = 0; i < subjectHeaders.length; i++) {
+        document.getElementById(subjectHeaders[i]).style.display = "none";
+    }
+    for (i = 0; i < subjects.length; i++) {
+        document.getElementById(subjects.name[i]).style.display = "none";
+    }
+}
+
+//Reveals all subject divs
+function showDivs() {
+    for (i = 0; i < subjectHeaders.length; i++) {
+        document.getElementById(subjectHeaders[i]).style.display = "Block";
+    }
+    for (i = 0; i < subjects.length; i++) {
+        document.getElementById(subjects.name[i]).style.display = "Block";
+    }
 }
