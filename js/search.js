55ed1349a439a188fd0780acd2de900187452909
oscian <oscar@reinitz.com.au>
2021-06-08 12:47:51 +1000

    Binary Search function completed and working. CSS centering for search page fixed. Div hide/show function error corrected.

--- js/search.js
+++ js/search.js
@@ -122,7 +122,7 @@ var subjects = [{
 
 var selectedSub = [];
 
-var subjectHeaders = ["headEn", "headMa", "headD", "headEc", "headG", "headH", "headL", "headMu", "headPdhpe", "headS", "headT", "headVa"]
+var subjectHeaders = ["headEn", "headMa", "headD", "headEc", "headG", "headH", "headL", "headMu", "headPdhpe", "headS", "headT", "headVa", "headP"]
 
 
 
@@ -131,53 +131,55 @@ function searchSub() {
     var searchTerm = document.getElementById("searchBar").value
     var tempTerm = ""
     var searchResult = ""
+
     //Sorts subjects alphabetically prior to binary search
-    sortsubjects()
+    subjects = sortsubjects()
 
     //Convert search term to its relative subject code for use in binary search
     tempTerm = simpTerm(searchTerm)
 
-    //Executes binary Search Function
-    searchResult = binarySearch(tempTerm)
+    //Validates search term
+    if (tempTerm == "invalid") {
+        searchResult = "null";
+    } else {
+        //Executes binary Search Function
+        searchResult = binarySearch(subjects, tempTerm)
+    }
 
-    if(searchResult == "null"){
+    //Displays search results
+    if (searchResult == "null") {
         alert("No Results")
-    }else{
+        showDivs()
+    } else {
         hideDivs()
 
-        document.getElementById(searchResult).style.display = "Block"
+        document.getElementById(subjects[searchResult].name).style.display = "Block"
     }
-    
+
 
 }
 
 //Binary Search Function
-function binarySearch(value) {
-    var startIndex = 0
-    var endIndex = subjects.length - 1
-    var middle = Math.floor((endIndex + startIndex) / 2)
+var binarySearch = function (items, value) {
+    var startIndex = 0,
+        stopIndex = items.length - 1,
+        middle = Math.floor((stopIndex + startIndex) / 2);
 
-    while(subjects[middle].name != value && startIndex < endIndex){
+    while (items[middle].name != value && startIndex < stopIndex) {
 
-        //Changes search location
-        if (value < subjects[middle].name) {
-            endIndex = middle - 1;
-        } else if (value > subjects[middle].name) {
-            endIndex = middle + 1;
+        //adjust search area
+        if (value < items[middle].name) {
+            stopIndex = middle - 1;
+        } else if (value > items[middle].name) {
+            startIndex = middle + 1;
         }
 
-        //Returns to centre of search
-        middle = Math.floor((endIndex + startIndex) / 2);
+        //recalculate middle
+        middle = Math.floor((stopIndex + startIndex) / 2);
     }
 
-    //Assures value is correct and returns it or null value
-    if(subjects[middle].name != value){
-        return("null")
-    }else{
-        return(subjects[middle].name)
-    }
-    
-    
+    //make sure it's the right value
+    return (items[middle].name != value) ? -1 : middle;
 }
 
 //Simplifies the search term to its relative code
@@ -191,116 +193,116 @@ function simpTerm(term) {
             return ("enEald");
         case "english standard":
         case "english (standard)":
-            return("enStand");
+            return ("enStand");
         case "english advanced":
         case "english (advanced)":
-            return("enAdv");
+            return ("enAdv");
         case "english extension 1":
-            return("enExt1");
+            return ("enExt1");
         case "english extension 2":
-            return("enExt2");
+            return ("enExt2");
         case "maths standard":
         case "mathematics standard":
-            return("maStand");
+            return ("maStand");
         case "maths advanced":
         case "advanced maths":
         case "mathematics advanced":
-            return("maAdv");
+            return ("maAdv");
         case "mathematics extension 1":
         case "maths extension 1":
-            return("maExt1");
+            return ("maExt1");
         case "mathematics extension 2":
         case "maths extension 2":
-            return("maExt2");
+            return ("maExt2");
         case "drama":
-            return("dDram");
+            return ("dDram");
         case "business":
         case "business studies":
-            return("ecBus");
+            return ("ecBus");
         case "economics":
-            return("ecEco");
+            return ("ecEco");
         case "geo":
         case "geography":
-            return("gGeo");
+            return ("gGeo");
         case "ancient history":
-            return("hAnc");
+            return ("hAnc");
         case "extension history":
         case "history extension":
-            return("hExt")
+            return ("hExt")
         case "legal":
         case "legal studies":
-            return("hLeg");
+            return ("hLeg");
         case "modern history":
-            return("hMod");
+            return ("hMod");
         case "external language":
         case "external language course":
-            return("lExt");
+            return ("lExt");
         case "french":
-            return("lFre");
+            return ("lFre");
         case "chinese":
-            return("lChi");
+            return ("lChi");
         case "latin":
-            return("lLat");
+            return ("lLat");
         case "modern greek":
         case "greek":
-            return("lGre");
+            return ("lGre");
         case "music 1":
-            return("mu1");
+            return ("mu1");
         case "music 2":
-            return("mu2");
+            return ("mu2");
         case "extension music":
         case "music extension":
-            return("muExt");
+            return ("muExt");
         case "pdhpe":
-            return("pdhpe");
+            return ("pdhpe");
         case "studies of religion i":
         case "studies of religion 1":
         case "sor 1":
-            return("pSr1");
+            return ("pSr1");
         case "studies of religion ii":
         case "studies of religion 2":
-        case "sor 2": 
-            return("pSr2");
+        case "sor 2":
+            return ("pSr2");
         case "bio":
         case "biology":
-            return("sBio");
+            return ("sBio");
         case "chem":
         case "chemistry":
-            return("sChe");
+            return ("sChe");
         case "earth and enviromental":
         case "earth and enviromental science":
-            return("sEes");
+            return ("sEes");
         case "physics":
-            return("sPhy");
+            return ("sPhy");
         case "extension science":
         case "science extension":
         case "year 12 hsc science extension":
-            return("sExt");
+            return ("sExt");
         case "dt":
         case "design and technology":
         case "design and tech":
         case "d and t":
         case "design technology":
         case "design tech":
-            return("tDt");
+            return ("tDt");
         case "engineering studies":
         case "engineering":
-            return("tEs");
+            return ("tEs");
         case "industrial technology":
         case "industrial tech":
-            return("tIt");
+            return ("tIt");
         case "ipt":
         case "information processes and technlogy":
-            return("tIpt");
+            return ("tIpt");
         case "sdd":
         case "software development and design":
-            return("tSdd");
+            return ("tSdd");
         case "art":
         case "va":
         case "visual arts":
-            return("va");        
-        
-
+            return ("va");
+        default:
+            return ("invalid");
     }
 }
 
@@ -308,19 +310,21 @@ function simpTerm(term) {
 
 //Sorts alphabetically the list of subjects
 function sortsubjects() {
-    subjects.sort(function (a, b) {
-        var x = a.name.toLowerCase();
-        var y = b.name.toLowerCase();
-        if (x < y) {
-            return -1;
-        }
-        if (x > y) {
-            return 1;
-        }
-        return 0;
-    });
 
-    console.log(subjects)
+    function compare(a, b) {
+        const thingA = a.name.toUpperCase();
+        const thingB = b.name.toUpperCase();
+
+        let comparison = 0;
+        if (thingA > thingB) {
+            comparison = 1;
+        } else if (thingA < thingB) {
+            comparison = -1;
+        }
+        return comparison;
+    }
+    return subjects.sort(compare);
+
 }
 
 //Hides all subject divs
@@ -329,7 +333,7 @@ function hideDivs() {
         document.getElementById(subjectHeaders[i]).style.display = "none";
     }
     for (i = 0; i < subjects.length; i++) {
-        document.getElementById(subjects.name[i]).style.display = "none";
+        document.getElementById(subjects[i].name).style.display = "none";
     }
 }
 
@@ -339,6 +343,6 @@ function showDivs() {
         document.getElementById(subjectHeaders[i]).style.display = "Block";
     }
     for (i = 0; i < subjects.length; i++) {
-        document.getElementById(subjects.name[i]).style.display = "Block";
+        document.getElementById(subjects[i].name).style.display = "Block";
     }
 }
