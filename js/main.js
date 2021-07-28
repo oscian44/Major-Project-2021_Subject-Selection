8b4c58bd7521acd69d214adf76eeef79c5ae80fc
oscian <59333603+oscian44@users.noreply.github.com>
2021-07-28 23:49:18 +1000

    Coded Validation for Submission Page

--- js/main.js
+++ js/main.js
@@ -219,7 +219,7 @@ function selectSubject() {
     localStorage.setItem("subjects", JSON.stringify(subjects));
 
     var subName1 = (subjects[num].name)
-    var subUnits1 = (subjects[num].units) + " Units"
+    var subUnits1 = (subjects[num].units)
 
     localStorage.setItem("subName1", subName1)
     localStorage.setItem("subUnits1", subUnits1)
@@ -237,7 +237,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName2 = (subjects[num].name)
-      var subUnits2 = (subjects[num].units) + " Units"
+      var subUnits2 = (subjects[num].units)
 
       localStorage.setItem("subName2", subName2)
       localStorage.setItem("subUnits2", subUnits2)
@@ -257,8 +257,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName3 = (subjects[num].name)
-      var subUnits3 = (subjects[num].units) + " Units"
-
+      var subUnits3 = (subjects[num].units)
       localStorage.setItem("subName3", subName3)
       localStorage.setItem("subUnits3", subUnits3)
 
@@ -276,7 +275,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName4 = (subjects[num].name)
-      var subUnits4 = (subjects[num].units) + " Units"
+      var subUnits4 = (subjects[num].units)
 
       localStorage.setItem("subName4", subName4)
       localStorage.setItem("subUnits4", subUnits4)
@@ -295,7 +294,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName5 = (subjects[num].name)
-      var subUnits5 = (subjects[num].units) + " Units"
+      var subUnits5 = (subjects[num].units)
 
       localStorage.setItem("subName5", subName5)
       localStorage.setItem("subUnits5", subUnits5)
@@ -314,7 +313,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName6 = (subjects[num].name)
-      var subUnits6 = (subjects[num].units) + " Units";
+      var subUnits6 = (subjects[num].units);
 
       localStorage.setItem("subName6", subName6)
       localStorage.setItem("subUnits6", subUnits6)
@@ -333,7 +332,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName7 = (subjects[num].name)
-      var subUnits7 = (subjects[num].units) + " Units"
+      var subUnits7 = (subjects[num].units)
 
       localStorage.setItem("subName7", subName7)
       localStorage.setItem("subUnits7", subUnits7)
@@ -352,7 +351,7 @@ function selectSubject() {
       localStorage.setItem("subjects", JSON.stringify(subjects));
 
       var subName8 = (subjects[num].name)
-      var subUnits8 = (subjects[num].units) + " Units"
+      var subUnits8 = (subjects[num].units)
 
       localStorage.setItem("subName8", subName8)
       localStorage.setItem("subUnits8", subUnits8)
@@ -367,53 +366,73 @@ function selectSubject() {
 
 function subChange() {
 
-  var subjectName1 = localStorage.getItem("subName1")
-  var subjectUnits1 = localStorage.getItem("subUnits1")
-  console.log(subjectName1 + " " + subjectUnits1 + " - Displayed on html as subject 1")
-  document.getElementById("sub1").innerHTML = subjectName1
-  document.getElementById("units1").innerHTML = subjectUnits1
+  if (localStorage.getItem("subUnits1") == null) {} else {
 
-  var subjectName2 = localStorage.getItem("subName2")
-  var subjectUnits2 = localStorage.getItem("subUnits2")
-  console.log(subjectName2 + " " + subjectUnits2 + " - Displayed on html as subject 2")
-  document.getElementById("sub2").innerHTML = subjectName2
-  document.getElementById("units2").innerHTML = subjectUnits2
+    var subjectName1 = localStorage.getItem("subName1")
+    var subjectUnits1 = localStorage.getItem("subUnits1")
+    console.log(subjectName1 + " " + subjectUnits1 + " - Displayed on html as subject 1")
+    document.getElementById("sub1").innerHTML = subjectName1
+    document.getElementById("units1").innerHTML = subjectUnits1 + " Units"
+  }
 
-  var subjectName3 = localStorage.getItem("subName3")
-  var subjectUnits3 = localStorage.getItem("subUnits3")
-  console.log(subjectName3 + " " + subjectUnits3 + " - Displayed on html as subject 3")
-  document.getElementById("sub3").innerHTML = subjectName3
-  document.getElementById("units3").innerHTML = subjectUnits3
+  if (localStorage.getItem("subUnits2") == null) {} else {
+    var subjectName2 = localStorage.getItem("subName2")
+    var subjectUnits2 = localStorage.getItem("subUnits2")
+    console.log(subjectName2 + " " + subjectUnits2 + " - Displayed on html as subject 2")
+    document.getElementById("sub2").innerHTML = subjectName2
+    document.getElementById("units2").innerHTML = subjectUnits2 + " Units"
+  }
 
-  var subjectName4 = localStorage.getItem("subName4")
-  var subjectUnits4 = localStorage.getItem("subUnits4")
-  console.log(subjectName4 + " " + subjectUnits4 + " - Displayed on html as subject 4")
-  document.getElementById("sub4").innerHTML = subjectName4
-  document.getElementById("units4").innerHTML = subjectUnits4
+  if (localStorage.getItem("subUnits3") == null) {} else {
+    var subjectName3 = localStorage.getItem("subName3")
+    var subjectUnits3 = localStorage.getItem("subUnits3")
+    console.log(subjectName3 + " " + subjectUnits3 + " - Displayed on html as subject 3")
+    document.getElementById("sub3").innerHTML = subjectName3
+    document.getElementById("units3").innerHTML = subjectUnits3 + " Units"
+  }
 
-  var subjectName5 = localStorage.getItem("subName5")
-  var subjectUnits5 = localStorage.getItem("subUnits5")
-  console.log(subjectName5 + " " + subjectUnits5 + " - Displayed on html as subject 5")
-  document.getElementById("sub5").innerHTML = subjectName5
-  document.getElementById("units5").innerHTML = subjectUnits5
+  if (localStorage.getItem("subUnits4") == null) {} else {
+    var subjectName4 = localStorage.getItem("subName4")
+    var subjectUnits4 = localStorage.getItem("subUnits4")
+    console.log(subjectName4 + " " + subjectUnits4 + " - Displayed on html as subject 4")
+    document.getElementById("sub4").innerHTML = subjectName4
+    document.getElementById("units4").innerHTML = subjectUnits4 + " Units"
+  }
 
-  var subjectName6 = localStorage.getItem("subName6")
-  var subjectUnits6 = localStorage.getItem("subUnits6")
-  console.log(subjectName6 + " " + subjectUnits7 + " - Displayed on html as subject 6")
-  document.getElementById("sub6").innerHTML = subjectName6
-  document.getElementById("units6").innerHTML = subjectUnits6
+  if (localStorage.getItem("subUnits5") == null) {} else {
+    var subjectName5 = localStorage.getItem("subName5")
+    var subjectUnits5 = localStorage.getItem("subUnits5")
+    console.log(subjectName5 + " " + subjectUnits5 + " - Displayed on html as subject 5")
+    document.getElementById("sub5").innerHTML = subjectName5
+    document.getElementById("units5").innerHTML = subjectUnits5 + " Units"
+  }
 
-  var subjectName7 = localStorage.getItem("subName7")
-  var subjectUnits7 = localStorage.getItem("subUnits7")
-  console.log(subjectName7 + " " + subjectUnits7 + " - Displayed on html as subject 7")
-  document.getElementById("sub7").innerHTML = subjectName7
-  document.getElementById("units7").innerHTML = subjectUnits7
+  if (localStorage.getItem("subUnits6") == null) {} else {
+    var subjectName6 = localStorage.getItem("subName6")
+    var subjectUnits6 = localStorage.getItem("subUnits6")
+    console.log(subjectName6 + " " + subjectUnits6 + " - Displayed on html as subject 6")
+    document.getElementById("sub6").innerHTML = subjectName6
+    document.getElementById("units6").innerHTML = subjectUnits6 + " Units"
+  }
 
-  var subjectName8 = localStorage.getItem("subName8")
-  var subjectUnits8 = localStorage.getItem("subUnits8")
-  console.log(subjectName8 + " " + subjectUnits8 + " - Displayed on html as subject 8")
-  document.getElementById("sub8").innerHTML = subjectName8
-  document.getElementById("units8").innerHTML = subjectUnits8
+  if (localStorage.getItem("subUnits7") == null) {} else {
+    var subjectName7 = localStorage.getItem("subName7")
+    var subjectUnits7 = localStorage.getItem("subUnits7")
+    console.log(subjectName7 + " " + subjectUnits7 + " - Displayed on html as subject 7")
+    document.getElementById("sub7").innerHTML = subjectName7
+    document.getElementById("units7").innerHTML = subjectUnits7 + " Units"
+  }
+
+  if (localStorage.getItem("subUnits8") == null) {} else {
+    var subjectName8 = localStorage.getItem("subName8")
+    var subjectUnits8 = localStorage.getItem("subUnits8")
+    console.log(subjectName8 + " " + subjectUnits8 + " - Displayed on html as subject 8")
+    document.getElementById("sub8").innerHTML = subjectName8
+    document.getElementById("units8").innerHTML = subjectUnits8 + " Units"
+  }
+  
+
+
 }
 
 function subClear() {
@@ -426,4 +445,149 @@ function subClear() {
     subjects[i].sel = 0
     localStorage.setItem("subjects", JSON.stringify(subjects));
   }
+  subChange()
+  location.reload()
+}
+
+function subSubmit() {
+  var sub1 = Number(localStorage.getItem("subUnits1"))
+  var sub2 = Number(localStorage.getItem("subUnits2"))
+  var sub3 = Number(localStorage.getItem("subUnits3"))
+  var sub4 = Number(localStorage.getItem("subUnits4"))
+  var sub5 = Number(localStorage.getItem("subUnits5"))
+  var sub6 = Number(localStorage.getItem("subUnits6"))
+  var sub7 = Number(localStorage.getItem("subUnits7"))
+  var sub8 = Number(localStorage.getItem("subUnits8"))
+
+  var subs = sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8
+
+  var sublist = [localStorage.getItem("subName1"), localStorage.getItem("subName2"), localStorage.getItem("subName3"), localStorage.getItem("subName4"), localStorage.getItem("subName5"), localStorage.getItem("subName6"), localStorage.getItem("subName7"), localStorage.getItem("subName8"), ]
+
+  var subN1 = localStorage.getItem("subName1")
+  var subN2 = localStorage.getItem("subName2")
+  var subN3 = localStorage.getItem("subName3")
+  var subN4 = localStorage.getItem("subName4")
+  var subN5 = localStorage.getItem("subName5")
+  var subN6 = localStorage.getItem("subName6")
+  var subN7 = localStorage.getItem("subName7")
+  var subN8 = localStorage.getItem("subName8")
+
+  var subNs1 = subN1.split(" ").shift()
+  var subNs2 = subN2.split(" ").shift()
+  var subNs3 = subN3.split(" ").shift()
+  var subNs4 = subN4.split(" ").shift()
+  var subNs5 = subN5.split(" ").shift()
+  var subNs6 = subN6.split(" ").shift()
+  var subNs7 = subN7.split(" ").shift()
+  var subNs8 = subN8.split(" ").shift()
+
+  var subNp1 = subN1.split(" ")
+  var subNp2 = subN2.split(" ")
+  var subNp3 = subN3.split(" ")
+  var subNp4 = subN4.split(" ")
+  var subNp5 = subN5.split(" ")
+  var subNp6 = subN6.split(" ")
+  var subNp7 = subN7.split(" ")
+  var subNp8 = subN8.split(" ")
+
+  var subNP = []
+
+  subNP = subNp1.concat(subNp2, subNp3, subNp4, subNp5, subNp6, subNp7, subNp8);
+
+  var subNS = [subNs1, subNs2, subNs3, subNs4, subNs5, subNs6, subNs7, subNs8]
+
+
+
+  switch (subs) {
+    case 0:
+    case null:
+      alert("Subject Selection Cannot Be Completely Empty")
+      break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+      alert("Units must total at least 10")
+      break;
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+      checkEnglish(subNS, subNP, sublist);
+
+
+
+  }
+}
+
+function checkEnglish(subNS, subNP, sublist) {
+  switch (countInArray(subNS, "English")) {
+    case 1:
+      if (countInArray(sublist, "English Extension 1") == 0 || countInArray(sublist, "English Extension 2") == 0) {
+        checkMaths(subNS, subNP, sublist)
+      } else {
+        alert("Only one subject of English May Be Selected")
+      }
+      break;
+    case 2:
+      if (countInArray(subNP, "Extension") >= 1 && countInArray(sublist, "English Standard") == 0) {
+        checkMaths(subNS, subNP, sublist)
+      } else {
+        alert("Only one subject of English May Be Selected")
+      }
+      break;
+    case 3:
+      if (countInArray(sublist, "English Extension 1") == 1 && countInArray(sublist, "English Extension 2") == 1) {
+        checkMaths(subNS, subNP, sublist)
+      } else {
+        alert("Only one subject of English May Be Selected")
+      }
+      break;
+    default:
+      alert("Only one subject of English May Be Selected")
+
+  }
+}
+
+function checkMaths(subNS, subNP, sublist) {
+  switch (countInArray(subNS, "Math")) {
+    case 0:
+      subDiv()
+      break;
+    case 1:
+      if (countInArray(sublist, "Math Extension 1") == 1 || countInArray(sublist, "Math Extension 2") == 1) {
+        alert("Only one subject of Maths May Be Selected")
+      }else{
+        subDiv()
+      }
+      break;
+    case 2:
+      if (countInArray(sublist, "Math Extension 1") == 1 && countInArray(sublist, "Math Extension 2") == 0){
+        subDiv()
+      }else{
+        alert("Only one subject of Maths May Be Selected")
+      }
+      break;
+      case 3:
+        if(countInArray(sublist, "Math Extension 1") == 1 || countInArray(sublist, "Math Extension 2") == 1){
+          subDiv()
+        }
+  }
+}
+
+function subDiv() {
+  document.getElementById("resultRes1").innerHTML = "Subjects Selection Valid"
+  document.getElementById("resultRes2").innerHTML = "Results Have Been Submitted For Review"
+}
+
+function countInArray(array, what) {
+  return array.filter(item => item == what).length;
 }
